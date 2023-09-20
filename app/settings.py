b11a91c817e0f5ec291aef83e3f77520c3033b39
ilyasosman <earlyosman@gmail.com>
2023-09-20 14:18:01 +0300

    static path

--- app/settings.py
+++ app/settings.py
@@ -151,7 +151,8 @@ STATIC_URL = '/static/'
 # MEDIA_URL = ''
 # MEDIA_ROOT = os.path.join(BASE_DIR, '')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
