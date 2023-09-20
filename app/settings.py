36b2e658d408231430a8be90551b2d2a9c3611a6
ilyasosman <earlyosman@gmail.com>
2023-09-20 13:55:05 +0300

    Comment out media root & url

--- app/settings.py
+++ app/settings.py
@@ -148,9 +148,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = ''
-MEDIA_ROOT = os.path.join(BASE_DIR, '')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# MEDIA_URL = ''
+# MEDIA_ROOT = os.path.join(BASE_DIR, '')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
