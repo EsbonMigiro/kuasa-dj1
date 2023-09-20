03f1cdd0fbdeec1bbb7361b14c4a3c514ca751c9
ilyasosman <earlyosman@gmail.com>
2023-09-20 16:43:18 +0300

    Dependecies

--- app/settings.py
+++ app/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -173,3 +174,5 @@ SIMPLE_JWT = {
 
 # Configure the user model
 AUTH_USER_MODEL = 'user.User'
+
+django_heroku.settings(locals())
